3c6e0ccd344f4b4fae343d951d76dfb18a367f7a
Francesco Gigli <francesco.gigli@timgroup.com>
2013-05-23 15:48:06 +0100

    Ready for testing!

--- js/main.js
+++ js/main.js
@@ -1,40 +1,91 @@
+var items = [
+    '"Project Gemini will save our bacon!" posted by Emily on the [company blog]. 2 minutes ago.',
+    'A [commit] on Project Gemini by Alfred with comment "Adapted to new button design". A week ago.',
+    '"Home Curing Of Bacon And Hams" has been [borrowed] by Edward. 9 days ago.',
+    'The wiki page ["updated design guidelines for the button widget"] was updated by Jack. 12 days ago.',
+    'Eric wrote on IRC: "git is AWESOME!" in channel [#gemini]. 14 days ago.',
+    'Maggie wrote on IRC: "we are working on a major breaktrough in the design of the application" in channel [#gemini]. 14 days ago.',
+    '"Principles for Successful Button Design" has been [borrowed] by Jack. A month ago.'
+    ];
+
+var at_home = true;
+
 function search(event) {
     event.preventDefault();
-    var results;
 
-    if($(".search-query").val() == "design") {
-	results = '<p>A <a href="#" class="link">commit</a> on Project Gemini by Alfred with comment "Adapted to new button design". A week ago.</p>' +
-	    '<p>The wiki page <a href="#" class="link">"updated design guidelines for the button widget"</a> was updated by Jack. 12 days ago.</p>' +
-	    '<p>Maggie wrote on IRC: "we are working on a major breaktrough in the design of the application" in channel <a href="#" class="link">#gemini</a>. 14 days ago.</p>' +
-	    '<p>"Principles for Successful Button Design" has been <a href="#" class="link">borrowed</a> by Jack. A month ago.</p>';
+    var html;
+    var query = $(".search-query").val();
+
+    var results = _.filter(items, function(item){ return item.toLowerCase().indexOf(query.toLowerCase()) != -1; });
+
+    if(query && results.length > 0) {
+        html = _.reduce(results, function(memo, result){
+            return memo + "<p>" + result.replace("[", '<a href="#" class="link">').replace("]", '</a>') + "</p>";
+        }, "");
     }
     else {
-	results = '<h3>No results found..</h3>' +
-	    '<p>Maybe try searching for something else?</p>' +
-	    '<p>Like <a href="#" class="hint">design</p>';
+        html = '<h3>No results found..</h3>' +
+            '<p>Maybe try searching for something else?</p>' +
+            '<p>Like <a href="#" class="hint">design</p>';
     }
 
     var meta = '<div class="meta">' +
-	'<span class="label label-info">Showing results for "' + $(".search-query").val() + '"</span>' +
-	'&nbsp;' +
-	'<a href="#" class="home">Return to the Dashboard</a>' +
-	'</div>';
+        '<span class="label label-info">Showing results for "' + $(".search-query").val() + '"</span>' +
+        '&nbsp;' +
+        '<a href="#" class="home">Return to the Dashboard</a>' +
+        '</div>';
 
-    $("#results").html(meta + results);
+    $("#results").html(meta + html);
+
+    at_home = false;
 }
 
 function go_home(event) {
-    var welcome = '<h1>Welcome to Dashboard!</h1>' +
-	'<h3>1: Search for something (eg. your name)</h3>' +
-	'<h3>2: Look at the results updating in real time</h3>' +
-	'<h3>3: Add to the watchlist the things you case for</h3>' +
-	'<p>' +
-	'Pssst.. You can even close this window and return later<br>' +
-	'we will remember and show your watchlist results.<br>' +
-	'But don\'t do that now! :-)' +
-	'</p>' +
-	'</div>';
-    $("#results").html(welcome);
+    var html;
+    var topics = get_watchlist_topics();
+
+    if(topics) {
+        var results = _.filter(items, function(item) {
+            return _.some(topics, function(topic) {
+                return item.toLowerCase().indexOf(topic.toLowerCase()) != -1;
+            });
+        });
+
+        var meta = '<div class="meta">' +
+            '<span class="label label-info">Showing results for your Watchlist</span>' +
+            '</div>';
+
+        if(results.length > 0) {
+            html = _.reduce(results, function(memo, result){
+                return memo + "<p>" + result.replace("[", '<a href="#" class="link">').replace("]", '</a>') + "</p>";
+            }, meta);
+
+            $("#results").html(html);
+
+            at_home=true;
+
+            return;
+        }
+    }
+
+    html = '<h1>Welcome to Dashboard!</h1>' +
+        '<h3>1: Search for something (eg. your name)</h3>' +
+        '<h3>2: Look at the results updating in real time</h3>' +
+        '<h3>3: Add to the watchlist the things you case for</h3>' +
+        '<p>' +
+        'Pssst.. You can even close this window and return later<br>' +
+        'we will remember and show your watchlist results.<br>' +
+        'But don\'t do that now! :-)' +
+        '</p>' +
+        '</div>';
+
+    $("#results").html(html);
+
+    at_home=true;
+}
+
+function get_watchlist_topics() {
+    return _.uniq(_.map(document.getElementsByClassName("pill"), function(p){ return p.textContent; }));
 }
 
 function hint(event) {
@@ -51,6 +102,9 @@ function link(event) {
 function add_term(term) {
     if(!term) return;
     $("#watchlist").append('<div class="pill">' + term + '<i class="delete-watchlist icon-remove"></i></div>');
+
+    if(at_home)
+        go_home(); // update results
 }
 
 function add_watchlist(event) {
@@ -73,7 +127,7 @@ function add_deletion_alert(term) {
     $("#alerts").append(alert);
     alert.alert();
     window.setTimeout(function() {
-	alert.alert('close');
+        alert.alert('close');
     }, 5000);
 }
 
@@ -83,6 +137,8 @@ function delete_watchlist(event) {
     var parent = $(this).parent();
     var term = parent.text();
     parent.remove();
+    if(at_home)
+        go_home(); // update results
     add_deletion_alert(term);
 }
 
